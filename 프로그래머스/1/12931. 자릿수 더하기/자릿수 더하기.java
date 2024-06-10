077a78979696c59510637968649570b165a2d36f
Jieun Kim <122339804+jin4618@users.noreply.github.com>
2024-06-11 01:13:42 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String ns = String.valueOf(n);
+        
+        for(int i = 0; i < ns.length(); i++) {
+            answer += Character.getNumericValue(ns.charAt(i));
+        }
+        
+        
+        
+        return answer;
+    }
+}
