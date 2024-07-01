47849e4a564a9e7a4bfef83d9ec8251821e45f0e
Jieun Kim <122339804+jin4618@users.noreply.github.com>
2024-07-02 02:13:42 +0900

    두 정수 사이의 합.java

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,7 +1,7 @@
 class Solution {
     public long solution(int a, int b) {
         long answer = 0;
-        /*int temp = 0;
+        int temp = 0;
         
         if(a > b) {
             temp = a;
@@ -11,16 +11,6 @@ class Solution {
         
         for(int i = a; i <= b; i++) {
             answer += i;
-        }*/
-        
-        if(a < b) {
-            for(int i = a; i <= b; i++) {
-                answer += i;
-            }
-        } else {
-            for(int i = b; i <= a; i++) {
-                answer += i;
-            }
         }
         
         return answer;
