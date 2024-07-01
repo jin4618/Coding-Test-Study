c0f625c9dd2d24434067669413c95ca6596f5d0a
Jieun Kim <122339804+jin4618@users.noreply.github.com>
2024-07-02 03:03:55 +0900

    [level 1] Title: 음양 더하기, Time: 0.86 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i < absolutes.length; i++) {
+            if(!signs[i]) {
+                absolutes[i] *= -1;
+            }
+        }
+        
+        answer = Arrays.stream(absolutes).sum();
+        
+        return answer;
+    }
+}
