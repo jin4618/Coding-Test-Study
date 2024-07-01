83ba903c1c7bcc0089ebdefe4ccf445e5f18f310
Jieun Kim <122339804+jin4618@users.noreply.github.com>
2024-07-02 03:47:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.67 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        
+        answer -= Arrays.stream(numbers).sum();
+        
+        return answer;
+    }
+}
