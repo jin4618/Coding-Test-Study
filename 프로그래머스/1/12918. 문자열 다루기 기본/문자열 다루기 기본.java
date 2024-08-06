4d94f102b5473ec8da67759c507016607d618cae
Jieun Kim <122339804+jin4618@users.noreply.github.com>
2024-08-06 22:26:59 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
         int len = s.length();
         
         if(len == 4 || len == 6) {
             for(int i = 0; i < len; i++) {
                 if(!Character.isDigit(s.charAt(i))) {
-                    return answer;
+                    return false;
                 }
             }
             return true;
         }
         
-        return answer;
+        return false;
     }
 }
