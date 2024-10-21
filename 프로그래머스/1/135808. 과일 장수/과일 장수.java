89a1be8eb47f2025090f3e5bc93bd0d5b8b17649
김지은 <122339804+jin4618@users.noreply.github.com>
2024-10-21 22:12:08 +0900

    [level 1] Title: 과일 장수, Time: 89.61 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -5,7 +5,6 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(score);    // 점수 정렬
-        int box = score.length / m;    // 만들 수 있는 상자 개수        
         
         for(int i = score.length - m; i >= 0; i -= m) { // 뒤부터 m개씩
             answer += score[i] * m;
