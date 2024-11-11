fcfccb1136e8b999a34730bfa0155aecd9052c30
김지은 <122339804+jin4618@users.noreply.github.com>
2024-11-11 21:58:53 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,23 +1,19 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
-        // 보드의 크기
         int n = board.length;
         
-        int[] dh = {0, 1, -1, 0};  // 상하좌우 h방향
-        int[] dw = {1, 0, 0, -1};  // 상하좌우 w방향
+        int[] dh = {0, 1, -1, 0};
+        int[] dw = {1, 0, 0, -1};
         
-        // 같은 색상의 개수를 카운트
         int count = 0;
-        
-        // 4방향 확인
+
         for (int i = 0; i < 4; i++) {
-            // 확인할 칸의 h, w 좌표 계산
+            
             int h_check = h + dh[i];
             int w_check = w + dw[i];
             
-            // 보드 범위 내에 있는지 확인
             if (h_check >= 0 && h_check < n && w_check >= 0 && w_check < n) {
-                // 같은 색상인지 확인
+                
                 if (board[h_check][w_check].equals(board[h][w])) {
                     count++;
                 }
