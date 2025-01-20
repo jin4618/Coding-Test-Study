60bdb05ef322bae2eae0da2916812de0b7295e70
김지은 <122339804+jin4618@users.noreply.github.com>
2025-01-20 14:58:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,18 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String base = "";
-        StringBuilder sb = new StringBuilder();
-
-        while (n != 0) {
-            base = (n % 3) + base;
-            n /= 3;
-        }
-
-        base = sb.append(base).reverse().toString();
-
-        answer = Integer.parseInt(base, 3);
-
-        return answer;
+        // 1. 3진법으로 변환
+        String ternary = Integer.toString(n, 3);
+        
+        // 2. 문자열 뒤집기
+        StringBuilder sb = new StringBuilder(ternary);
+        String reversed = sb.reverse().toString();
+        
+        // 3. 10진수로 변환
+        return Integer.parseInt(reversed, 3);
     }
 }
