a1a049550803ddfa7ae387aa756c0fbeeafe4b11
김지은 <122339804+jin4618@users.noreply.github.com>
2025-02-03 14:57:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,37 +1,13 @@
-import java.util.*;
-
 class Solution {
+    private static final String[] words = {
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
+    
     public int solution(String s) {
-        
-        Map<String, String> engNums = new HashMap<>();
-        
-        engNums.put("zero", "0");
-        engNums.put("one", "1");
-        engNums.put("two", "2");
-        engNums.put("three", "3");
-        engNums.put("four", "4");
-        engNums.put("five", "5");
-        engNums.put("six", "6");
-        engNums.put("seven", "7");
-        engNums.put("eight", "8");
-        engNums.put("nine", "9");
-
-        StringBuilder result = new StringBuilder();
-        StringBuilder check = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                result.append(c);
-            } else {
-                check.append(c);
-                String word = check.toString();
-                if (engNums.containsKey(word)) {
-                    result.append(engNums.get(word));
-                    check.setLength(0);
-                }
-            }
+        for(int i = 0; i < words.length; i++) {
+            s = s.replace(words[i], Integer.toString(i));
         }
-
-        return Integer.parseInt(result.toString());
+        
+        return Integer.parseInt(s);
     }
 }
