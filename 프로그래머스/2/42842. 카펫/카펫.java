bbd60b346280f73fccc4a04dd17a5ffbf608ec67
김지은 <122339804+jin4618@users.noreply.github.com>
2025-03-03 19:18:45 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        for (int yellowHeight = 1; yellowHeight <= Math.sqrt(yellow); yellowHeight++) {
+            if (yellow % yellowHeight != 0) continue;
+            
+            int yellowWidth = yellow / yellowHeight;
+            
+            // 전체 카펫 크기
+            int totalHeight = yellowHeight + 2;
+            int totalWidth = yellowWidth + 2;
+            
+            // 갈색 격자 수 계산 및 확인
+            int calculatedBrown = totalHeight * totalWidth - yellow;
+            
+            if (calculatedBrown == brown && totalWidth >= totalHeight) {
+                return new int[] {totalWidth, totalHeight};
+            }
+        }
+        
+        return new int[] {0, 0};
+    }
+}
